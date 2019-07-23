6d394f5c8549cec69b8e20d400c72c01bcf3416c
owuordickson <owuordickson@gmail.com>
2019-07-23 20:02:18 +0300

    created class

--- algorithms/data_set.py
+++ algorithms/data_set.py
@@ -9,12 +9,21 @@
 import csv
 
 
-def read_csv(file):
-    # 1. retrieve data-set from file
-    with open(file, 'r') as f:
-        dialect = csv.Sniffer().sniff(f.read(1024), delimiters=";,' '\t")
-        f.seek(0)
-        reader = csv.reader(f, dialect)
-        temp = list(reader)
-        f.close()
-    return temp
+class DataSet:
+
+    def __init__(self, file_path):
+        raw_data = DataSet.read_csv(file_path)
+
+    def separate_columns(self):
+        return self.raw_data
+
+    @staticmethod
+    def read_csv(file):
+        # 1. retrieve data-set from file
+        with open(file, 'r') as f:
+            dialect = csv.Sniffer().sniff(f.read(1024), delimiters=";,' '\t")
+            f.seek(0)
+            reader = csv.reader(f, dialect)
+            temp = list(reader)
+            f.close()
+        return temp
