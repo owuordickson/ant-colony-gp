e956d4fde0193ad0c9220953454b898641b0a01f
owuordickson <owuordickson@gmail.com>
2019-09-16 10:46:26 +0200

    added setup files

---
+++ AUTHORS.rst
@@ -0,0 +1,12 @@
+Credits:
+--------
+1. Thomas Runkler <thomas.runkler@siemens.com>
+2. Anne Laurent <laurent@lirmm.fr>
+3. Edmond Menya <emenya@strathmore.edu>
+
+Development Lead:
+-----------------
+1. Dickson Owuor <owuordickson@ieee.org>
+
+Contributors
+------------
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read()
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+
+setup(
+    name='aco-graank',
+    version='1.0',
+    description="A Python implementation of ant colony optimization of the GRAANK algorithm.",
+    long_description=readme + '\n\n' + history,
+    author="Dickson Owuor",
+    author_email='owuordickson@ieee.org',
+    url='https://github.com/owuordickson/ant-colony-gp',
+    packages=find_packages(),
+    #package_dir={'tgraank':'tgraank'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='aco-graank',
+    classifiers=[
+        'Development Status :: 1 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Massachusetts Institute of Technology (MIT) License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
