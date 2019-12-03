17c3783568ab26d85757d63130590a9a0d259e89
owuordickson <owuordickson@gmail.com>
2019-12-03 15:09:23 +0300

    changed path

--- src/init_acotgraank.py
+++ src/init_acotgraank.py
@@ -132,5 +132,5 @@ if __name__ == "__main__":
     wr_text = ("Run-time: " + str(end - start) + " seconds\n")
     wr_text += str(res_text)
     f_name = ('res_aco' + str(end).replace('.', '', 1) + '.txt')
-    HandleData.write_file(wr_text, '../data/' + f_name)
+    HandleData.write_file(wr_text, f_name)
     print(wr_text)
