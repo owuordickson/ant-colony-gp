4039c3dea28233c8a845565e78c3198f3b5d9f17
owuordickson <owuordickson@gmail.com>
2020-06-22 14:53:13 +0300

    updated

--- src/algorithms/ant_colony/hdf5/aco_tgrad_h5.py
+++ src/algorithms/ant_colony/hdf5/aco_tgrad_h5.py
@@ -16,6 +16,7 @@ Description: updated version that uses aco-graank and parallel multi-processing
 import numpy as np
 from ...common.hdf5.dataset_h5 import Dataset_h5
 from .aco_grad_h5 import GradACO_h5
+from ..aco_tgrad import T_GradACO
 from ...common.fuzzy_mf import calculate_time_lag
 from ...common.gp import GP, TGP
 from src.algorithms.common.profile_cpu import Profile
@@ -71,7 +72,7 @@ class GradACOt_h5 (GradACO_h5):
             return tgp
 
 
-class T_GradACO_h5:
+class T_GradACO_h5(T_GradACO):
 
     def __init__(self, f_path, eq, ref_item, min_sup, min_rep, cores):
         # For tgraank
@@ -99,18 +100,6 @@ class T_GradACO_h5:
             self.time_cols = []
             raise Exception('No date-time data found')
 
-    def get_max_step(self, min_rep):  # optimized
-        all_rows = len(self.d_set.data)
-        return all_rows - int(min_rep * all_rows)
-
-    def run_tgraank(self):
-        patterns = list()
-        for step in range(self.max_step):
-            t_pattern = self.fetch_patterns(step)
-            if t_pattern:
-                patterns.append(t_pattern)
-        return patterns
-
     def fetch_patterns(self, step):
         step += 1  # because for-loop is not inclusive from range: 0 - max_step
         # 1. Transform data
@@ -125,70 +114,3 @@ class T_GradACO_h5:
         if len(list_gp) > 0:
             return list_gp
         return False
-
-    def transform_data(self, step):  # optimized
-        # NB: Restructure dataset based on reference item
-        if self.time_ok:
-            # 1. Calculate time difference using step
-            ok, time_diffs = self.get_time_diffs(step)
-            if not ok:
-                msg = "Error: Time in row " + str(time_diffs[0]) \
-                      + " or row " + str(time_diffs[1]) + " is not valid."
-                raise Exception(msg)
-            else:
-                ref_col = self.ref_item
-                if ref_col in self.time_cols:
-                    msg = "Reference column is a 'date-time' attribute"
-                    raise Exception(msg)
-                elif (ref_col < 0) or (ref_col >= len(self.d_set.title)):
-                    msg = "Reference column does not exist\nselect column between: " \
-                          "0 and " + str(len(self.d_set.title) - 1)
-                    raise Exception(msg)
-                else:
-                    # 1. Split the transpose data set into column-tuples
-                    attr_data = self.orig_attr_data
-
-                    # 2. Transform the data using (row) n+step
-                    new_attr_data = list()
-                    size = len(attr_data)
-                    for k in range(size):
-                        col_index = k
-                        tuples = attr_data[k]
-                        n = tuples.size
-                        # temp_tuples = np.empty(n, )
-                        # temp_tuples[:] = np.NaN
-                        if col_index in self.time_cols:
-                            # date-time attribute
-                            temp_tuples = tuples[:]
-                        elif col_index == ref_col:
-                            # reference attribute
-                            temp_tuples = tuples[0: n - step]
-                        else:
-                            # other attributes
-                            temp_tuples = tuples[step: n]
-                        # print(temp_tuples)
-                        new_attr_data.append(temp_tuples)
-                    return new_attr_data, time_diffs
-        else:
-            msg = "Fatal Error: Time format in column could not be processed"
-            raise Exception(msg)
-
-    def get_time_diffs(self, step):  # optimized
-        data = self.d_set.data
-        size = len(data)
-        time_diffs = []
-        for i in range(size):
-            if i < (size - step):
-                # for col in self.time_cols:
-                col = self.time_cols[0]  # use only the first date-time value
-                temp_1 = str(data[i][int(col)])
-                temp_2 = str(data[i + step][int(col)])
-                stamp_1 = Dataset_h5.get_timestamp(temp_1)
-                stamp_2 = Dataset_h5.get_timestamp(temp_2)
-                if (not stamp_1) or (not stamp_2):
-                    return False, [i + 1, i + step + 1]
-                time_diff = (stamp_2 - stamp_1)
-                # index = tuple([i, i + step])
-                # time_diffs.append([time_diff, index])
-                time_diffs.append([time_diff, i])
-        return True, np.array(time_diffs)
--- src/algorithms/graank/hdf5/t_graank_h5.py
+++ src/algorithms/graank/hdf5/t_graank_h5.py
@@ -13,14 +13,14 @@ Description: updated version that uses aco-graank and parallel multi-processing
 
 """
 
-# from joblib import Parallel, delayed
 import numpy as np
 from ...common.hdf5.dataset_h5 import Dataset_h5
+from ..t_graank import Tgrad
 from ...common.profile_cpu import Profile
 from .graank_h5 import graank_h5
 
 
-class Tgrad_5:
+class Tgrad_5(Tgrad):
 
     def __init__(self, f_path, eq, ref_item, min_sup, min_rep, cores):
         # For tgraank
@@ -48,18 +48,6 @@ class Tgrad_5:
             self.time_cols = []
             raise Exception('No date-time data found')
 
-    def get_max_step(self, min_rep):  # optimized
-        all_rows = len(self.d_set.data)
-        return all_rows - int(min_rep * all_rows)
-
-    def run_tgraank(self):
-        patterns = list()
-        for step in range(self.max_step):
-            t_pattern = self.fetch_patterns(step)
-            if t_pattern:
-                patterns.append(t_pattern)
-        return patterns
-
     def fetch_patterns(self, step):
         step += 1  # because for-loop is not inclusive from range: 0 - max_step
         # 1. Transform data
@@ -73,70 +61,3 @@ class Tgrad_5:
         if len(tgps) > 0:
             return tgps
         return False
-
-    def transform_data(self, step):  # optimized
-        # NB: Restructure dataset based on reference item
-        if self.time_ok:
-            # 1. Calculate time difference using step
-            ok, time_diffs = self.get_time_diffs(step)
-            if not ok:
-                msg = "Error: Time in row " + str(time_diffs[0]) \
-                      + " or row " + str(time_diffs[1]) + " is not valid."
-                raise Exception(msg)
-            else:
-                ref_col = self.ref_item
-                if ref_col in self.time_cols:
-                    msg = "Reference column is a 'date-time' attribute"
-                    raise Exception(msg)
-                elif (ref_col < 0) or (ref_col >= len(self.d_set.title)):
-                    msg = "Reference column does not exist\nselect column between: " \
-                          "0 and " + str(len(self.d_set.title) - 1)
-                    raise Exception(msg)
-                else:
-                    # 1. Split the transpose data set into column-tuples
-                    attr_data = self.orig_attr_data
-
-                    # 2. Transform the data using (row) n+step
-                    new_attr_data = list()
-                    size = len(attr_data)
-                    for k in range(size):
-                        col_index = k
-                        tuples = attr_data[k]
-                        n = tuples.size
-                        # temp_tuples = np.empty(n, )
-                        # temp_tuples[:] = np.NaN
-                        if col_index in self.time_cols:
-                            # date-time attribute
-                            temp_tuples = tuples[:]
-                        elif col_index == ref_col:
-                            # reference attribute
-                            temp_tuples = tuples[0: n - step]
-                        else:
-                            # other attributes
-                            temp_tuples = tuples[step: n]
-                        # print(temp_tuples)
-                        new_attr_data.append(temp_tuples)
-                    return new_attr_data, time_diffs
-        else:
-            msg = "Fatal Error: Time format in column could not be processed"
-            raise Exception(msg)
-
-    def get_time_diffs(self, step):  # optimized
-        data = self.d_set.data
-        size = len(data)
-        time_diffs = []
-        for i in range(size):
-            if i < (size - step):
-                # for col in self.time_cols:
-                col = self.time_cols[0]  # use only the first date-time value
-                temp_1 = str(data[i][int(col)])
-                temp_2 = str(data[i + step][int(col)])
-                stamp_1 = Dataset_h5.get_timestamp(temp_1)
-                stamp_2 = Dataset_h5.get_timestamp(temp_2)
-                if (not stamp_1) or (not stamp_2):
-                    return False, [i + 1, i + step + 1]
-                time_diff = (stamp_2 - stamp_1)
-                # index = tuple([i, i + step])
-                # time_diffs.append([time_diff, index])
-                time_diffs.append([time_diff, i])
-        return True, np.array(time_diffs)
