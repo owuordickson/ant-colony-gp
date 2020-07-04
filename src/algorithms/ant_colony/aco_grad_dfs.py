bfd8e28cad105ea18d2c0d81ce3dfedfc80ec632
owuordickson <owuordickson@gmail.com>
2020-07-04 23:41:36 +0300

    encoding data for DFS

--- src/algorithms/ant_colony/aco_grad_dfs.py
+++ src/algorithms/ant_colony/aco_grad_dfs.py
@@ -26,7 +26,7 @@ class GradACO:
         self.c_matrix = self.d_set.cost_matrix
         self.p_matrix = np.ones((self.d_set.column_size, 3), dtype=int)
         # Data set reduction and update: (p_matrix, attr_index)
-        #self.reduce_data()
+        self.reduce_data()
         self.attr_index = self.d_set.attr_cols
         # self.e_factor = 0.1  # evaporation factor
         print(self.d_set.encoded_data)
@@ -56,19 +56,22 @@ class GradACO:
         vals, inverse, count = np.unique(self.d_set.encoded_data[:, 2:],
                                          return_inverse=True,
                                          return_counts=True, axis=0)
-        idx_vals_repeated = np.where(count > 1)[0]
-        rows, cols = np.where(inverse == idx_vals_repeated[:, np.newaxis])
-        _, inverse_rows = np.unique(rows, return_index=True)
-        res = np.split(cols, inverse_rows[1:])
+        #idx_vals_repeated = np.where(count > 1)[0]
+        #rows, cols = np.where(inverse == idx_vals_repeated[:, np.newaxis])
+        #_, inverse_rows = np.unique(rows, return_index=True)
+        #res = np.split(cols, inverse_rows[1:])
+        #print(res)
+        print(vals)
+        print(count)
         # 2b. use indices to merge
-        if len(res) > 1:
-            n_data = list()
-            for obj in res:
-                row = self.d_set.encoded_data[obj]
-                nodes = row[:, [0, 1]]
-                data = row[0, 2:]
-                n_data.append([nodes, data])
-            self.d_set.encoded_data = np.array(n_data)
+        # if len(res) > 1:
+        #    n_data = list()
+        #    for obj in res:
+        #        row = self.d_set.encoded_data[obj]
+        #        nodes = row[:, [0,1]]
+        #        data = row[0, 2:]
+        #        n_data.append([nodes, data])
+        #    self.d_set.encoded_data = np.array(n_data)
 
     def run_ant_colony(self):
         min_supp = self.d_set.thd_supp
