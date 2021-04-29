638353aa14ff8fab992eba7f92c04d31605eb58c
owuordickson <owuordickson@gmail.com>
2021-04-29 22:55:29 +0300

    updated

---
+++ src/algorithms/ga/ga_grad.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+@author: "Dickson Owuor"
+@credits: "Thomas Runkler, Edmond Menya, and Anne Laurent,"
+@license: "MIT"
+@version: "1.0"
+@email: "owuordickson@gmail.com"
+@created: "29 April 2021"
+@modified: "29 April 2021"
+
+Breath-First Search for gradual patterns (GA-GRAANK)
+
+"""
+import numpy as np
+from src.common.gp import GI, GP
+from src.common.dataset_bfs import Dataset
+
+
+class GradGA:
+
+    def __init__(self, f_path, min_supp):
+        self.d_set = Dataset(f_path, min_supp)
+        self.d_set.init_gp_attributes()
+        self.attr_index = self.d_set.attr_cols
+        self.iteration_count = 0
+        self.d, self.attr_keys = self.generate_d()  # distance matrix (d) & attributes corresponding to d
+
+    def generate_d(self):
+        v_bins = self.d_set.valid_bins
+        # 1. Fetch valid bins group
+        attr_keys = [GI(x[0], x[1].decode()).as_string() for x in v_bins[:, 0]]
+
+        # 2. Initialize an empty d-matrix
+        n = len(attr_keys)
+        d = np.zeros((n, n), dtype=np.dtype('i8'))  # cumulative sum of all segments
+        for i in range(n):
+            for j in range(n):
+                if GI.parse_gi(attr_keys[i]).attribute_col == GI.parse_gi(attr_keys[j]).attribute_col:
+                    # Ignore similar attributes (+ or/and -)
+                    continue
+                else:
+                    bin_1 = v_bins[i][1]
+                    bin_2 = v_bins[j][1]
+                    # Cumulative sum of all segments for 2x2 (all attributes) gradual items
+                    d[i][j] += np.sum(np.multiply(bin_1, bin_2))
+        # print(d)
+        return d, attr_keys
+
+    @staticmethod
+    def check_anti_monotony(lst_p, pattern, subset=True):
+        result = False
+        if subset:
+            for pat in lst_p:
+                result1 = set(pattern.get_pattern()).issubset(set(pat.get_pattern()))
+                result2 = set(pattern.inv_pattern()).issubset(set(pat.get_pattern()))
+                if result1 or result2:
+                    result = True
+                    break
+        else:
+            for pat in lst_p:
+                result1 = set(pattern.get_pattern()).issuperset(set(pat.get_pattern()))
+                result2 = set(pattern.inv_pattern()).issuperset(set(pat.get_pattern()))
+                if result1 or result2:
+                    result = True
+                    break
+        return result
+
+    @staticmethod
+    def is_duplicate(pattern, lst_winners, lst_losers):
+        for pat in lst_losers:
+            if set(pattern.get_pattern()) == set(pat.get_pattern()) or \
+                    set(pattern.inv_pattern()) == set(pat.get_pattern()):
+                return True
+        for pat in lst_winners:
+            if set(pattern.get_pattern()) == set(pat.get_pattern()) or \
+                    set(pattern.inv_pattern()) == set(pat.get_pattern()):
+                return True
+        return False
