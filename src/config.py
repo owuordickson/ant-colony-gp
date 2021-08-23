4104482c75443bd9c9496f0ac5fc56a2392b334f
owuordickson <owuordickson@gmail.com>
2021-08-23 19:36:20 +0300

    updated

--- src/config.py
+++ src/config.py
@@ -42,6 +42,6 @@ N_PARTICLES = 5
 STEP_SIZE = 0.5
 
 # VISUALIZATIONS
-SHOW_P_MATRIX = False  # FOR aco
+SHOW_P_MATRIX = False  # FOR aco ONLY
 SHOW_EVALUATIONS = False  # FOR aco, prs, pls, pso
 SHOW_ITERATIONS = False  # FOR aco, prs, pls, pso
